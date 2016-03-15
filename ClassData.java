43089660822025543507e9df938950742ca24ea2
Bilal Reza <rezasb@s.dcsdk12.org>
2016-03-15 10:14:52 -0600

    Set up runner

---
+++ ClassData.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*
+
+public class ClassData {
+	String fileName;
+	
+	public Data(string fileName) {
+		this.fileName = fileName;
+	}
+
+	public static ArrayList<Team> getTeams() {
+		try {
+			File(file) = new file(ncaa_data_2016.csv);
+			Scanner input = new Scanner(file);
+
+			ArrayList<Team> teams = new ArrayList<Teams>;
+			
+
+		}
+	}
+	
+}
